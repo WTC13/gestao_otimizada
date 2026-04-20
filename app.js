f598f529ffbe05e1920b55c7ee77c42f23884b3c
Wendell Costa <wendell.timoteoc@gmail.com>
2026-04-20 12:09:27 -0300

    update project

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ const { verificarLogin } = require("./middleware/auth_middleware");
 // const port = process.env.PORT;
 
 
-app.engine("handlebars", hbs.engine({extname: ".handlebars"}));
+app.engine("handlebars", hbs.engine({
+    extname: ".handlebars",
+    defaultLayout: "main"
+}));
+
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
